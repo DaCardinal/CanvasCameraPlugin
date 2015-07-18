fd18f5599daebb2bb74989cb1c71c8c0d06d949c
DaCardinal <Daniel@Macbook-Pro-2.local>
2015-07-18 16:43:21 -0400

    Fixed prototype error

--- www/CanvasCamera.js
+++ www/CanvasCamera.js
@@ -9,52 +9,50 @@ cordova.define("cordova/plugin/CanvasCamera", function(require, exports, module)
     var argscheck = require('cordova/argscheck');
     var	utils = require('cordova/utils');
 
-    var PLUGIN_NAME = "CanvasCamera";
-
     var CanvasCamera = function(){
     };
 
-    CanvasCamera.setOnPictureTakenHandler = function(onPictureTaken) {
-    	exec(onPictureTaken, onPictureTaken, PLUGIN_NAME, "setOnPictureTakenHandler", []);
+    CanvasCamera.prototype.setOnPictureTakenHandler = function(onPictureTaken) {
+    	exec(onPictureTaken, onPictureTaken, "CanvasCamera", "setOnPictureTakenHandler", []);
     };
 
     //@param rect {x: 0, y: 0, width: 100, height:100}
     //@param defaultCamera "front" | "back"
-    CanvasCamera.startCamera = function(rect, defaultCamera, tapEnabled, dragEnabled, toBack) {
-    	exec(null, null, PLUGIN_NAME, "startCamera", [rect.x, rect.y, rect.width, rect.height, defaultCamera, !!tapEnabled, !!dragEnabled, !!toBack]);
+    CanvasCamera.prototype.startCamera = function(rect, defaultCamera, tapEnabled, dragEnabled, toBack) {
+    	exec(null, null, "CanvasCamera", "startCamera", [rect.x, rect.y, rect.width, rect.height, defaultCamera, !!tapEnabled, !!dragEnabled, !!toBack]);
     };
 
-    CanvasCamera.stopCamera = function() {
-    	exec(null, null, PLUGIN_NAME, "stopCamera", []);
+    CanvasCamera.prototype.stopCamera = function() {
+    	exec(null, null, "CanvasCamera", "stopCamera", []);
     };
 
     //@param size {maxWidth: 100, maxHeight:100}
-    CanvasCamera.takePicture = function(size) {
+    CanvasCamera.prototype.takePicture = function(size) {
     	var params = [0, 0];
     	if(size){
     		params = [size.maxWidth, size.maxHeight];
     	}
-    	exec(null, null, PLUGIN_NAME, "takePicture", params);
+    	exec(null, null, "CanvasCamera", "takePicture", params);
     };
 
-    CanvasCamera.setColorEffect = function(effect) {
-      exec(null, null, PLUGIN_NAME, "setColorEffect", [effect]);
+    CanvasCamera.prototype.setColorEffect = function(effect) {
+      exec(null, null, "CanvasCamera", "setColorEffect", [effect]);
     };
 
-    CanvasCamera.switchCamera = function() {
-    	exec(null, null, PLUGIN_NAME, "switchCamera", []);
+    CanvasCamera.prototype.switchCamera = function() {
+    	exec(null, null, "CanvasCamera", "switchCamera", []);
     };
 
-    CanvasCamera.hide = function() {
-    	exec(null, null, PLUGIN_NAME, "hideCamera", []);
+    CanvasCamera.prototype.hide = function() {
+    	exec(null, null, "CanvasCamera", "hideCamera", []);
     };
 
-    CanvasCamera.show = function() {
-    	exec(null, null, PLUGIN_NAME, "showCamera", []);
+    CanvasCamera.prototype.show = function() {
+    	exec(null, null, "CanvasCamera", "showCamera", []);
     };
 
-    CanvasCamera.disable = function(disable) {
-    	exec(null, null, PLUGIN_NAME, "disable", [disable]);
+    CanvasCamera.prototype.disable = function(disable) {
+    	exec(null, null, "CanvasCamera", "disable", [disable]);
     };
 
     var myplugin = new CanvasCamera();
